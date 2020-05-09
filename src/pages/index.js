a09fd851298b329e5d6622b64398e21b010523bc
Glitch (401k) <none>
2020-05-09 23:17:39 +0000

    💒🚒 Updated with Glitch

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,75 @@
-import React from 'react'
-import { Link } from 'gatsby'
+import React from "react"
+import { css } from "@emotion/core"
+import { Link, graphql } from "gatsby"
+import Layout from "../components/layout"
+import { Box, Button, Heading, Text, Image, useColorMode } from "@chakra-ui/core";
+import { Helmet } from "react-helmet";
 
-export default () => {
+export default function IndexPage({ data }) {
+  const { toggleColorMode } = useColorMode();
   return (
-    <>
-      Hello world!
-      <p><Link to="/blog">View Blog</Link></p>
-      <p><Link to="/account">My Account</Link></p>
-    </>)
+    <Layout>
+      <Helmet>
+        <meta name='description' content='Creating masterful 3D multiplayer games!'></meta>
+        <meta property='og:title' content="CS1 Blog"/>
+        <meta property='og:image' content="//cdn.glitch.com/53f6eb06-66f0-4243-b635-db0bfec50c28%2FCS1_Banner.png?v=1589051460395"/>
+        <meta property="og:description" content="Creating masterful 3D multiplayer games!"/>
+        <meta property="og:url" content="//401k.glitch.me" />
+      </Helmet>
+      <Box>
+        <Image src="https://cdn.glitch.com/53f6eb06-66f0-4243-b635-db0bfec50c28%2FCS1_Banner.png?v=1589051460395"/>
+        <Heading>CS1 Blog</Heading>
+        <hr></hr>
+        <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
+        {data.allMarkdownRemark.edges.map(({ node }) => (
+          <Box  maxW="sm" key={node.id}>
+            <Link
+              to={node.fields.slug}
+              css={css`
+                text-decoration: none;
+                color: inherit;
+              `}
+            >
+              <Heading as="h6" size="xs">
+                {node.frontmatter.title}{" "}
+                <span
+                  css={css`
+                    color: #555;
+                  `}
+                >
+                  — {node.frontmatter.date}
+                </span>
+              </Heading>
+              <div>{node.excerpt}</div>
+            </Link>
+          </Box>
+        ))}
+        
+    
+        
+      </Box>
+      
+    </Layout>
+  )
 }
+
+export const query = graphql`
+  query {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD MMMM, YYYY")
+          }
+          fields {
+            slug
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
