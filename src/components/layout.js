7a0f41fe0b7f6a94ba81ac35cb313267dd7b9b8d
Glitch (acidic-great-secure) <none>
2020-06-29 23:41:47 +0000

    add NoFlashScript

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,11 +7,13 @@ import {
   useColorMode,
   IconButton
 } from "@chakra-ui/core";
+import NoFlashScript from "./NoFlashScript.js"
 
 export default function Layout({ children }) {
   const { toggleColorMode } = useColorMode();
   return (
     <div style={{ margin: `3rem auto`, maxWidth: 650, padding: `0 1rem` }}>
+      <NoFlashScript></NoFlashScript>
       {children}
       <br></br>
       <Button mt={25} variantColor="blue" onClick={toggleColorMode}>
